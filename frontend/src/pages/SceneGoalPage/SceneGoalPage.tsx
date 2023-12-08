bb73cce23960ea76937944c96554f0143fa97901
Alexander Unmuth <alexander.unmuth@gmail.com>
2023-12-08 14:47:19 +0100

    Refactoring components scenes. Adapting the respective styling. delete locationstate.ts delte typing.d.ts

--- frontend/src/pages/SceneGoalPage/SceneGoalPage.tsx
+++ frontend/src/pages/SceneGoalPage/SceneGoalPage.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import './SceneOnePage.css';
-import "../../App.css";
+import SceneLayout from '../../components/SceneLayout/SceneLayout.tsx';
+import backgroundImage from '../../assets/sceneGoal.png';
+import {useLocation} from "react-router-dom";
+
+interface NavLink {
+    path: string;
+    label: string;
+}
 
 const SceneGoalPage: React.FC = () => {
+    const navLinks: NavLink[] = [];
+
+    const location = useLocation();
+
     return (
-        <main className="goalPageContainer">
-            <header>
-                <h1 className="startPageTitle">Szene Goal - Gewonnen</h1>
-            </header>
+        <SceneLayout
+            title="Szene Goal - Gewonnen"
+            navLinks={navLinks}
+            backgroundImage={backgroundImage}
+        >
 
-
-            <article className="textWindow">
-                <p>Im nebelverhangenen Sumpf, wo du zuvor einen Skelettarm entdeckt hattest, fandest du dich plötzlich feststeckend wieder. In einem Moment der Entschlossenheit ergriffst du den Arm, und überraschenderweise zog diese Handlung dich aus deiner misslichen Lage. Der Goldtopf in den Händen des Skeletts, unerwartet, markierte einen unvergesslichen Wendepunkt in deinem Abenteuer.</p>
-            </article>
-        </main>
+            <p>Aktuelle Location: {location.pathname}</p>
+            <p>Im nebelverhangenen Sumpf, wo du zuvor einen Skelettarm entdeckt hattest, fandest du dich plötzlich
+                feststeckend wieder. In einem Moment der Entschlossenheit ergriffst du den Arm, und überraschenderweise
+                zog diese Handlung dich aus deiner misslichen Lage. Der Goldtopf in den Händen des Skeletts, unerwartet,
+                markierte einen unvergesslichen Wendepunkt in deinem Abenteuer.</p>
+        </SceneLayout>
     );
 };
-
 
 export default SceneGoalPage;
