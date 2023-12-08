c584503c27f0ca39d77f3591e5364f060d1cf50a
Alexander Unmuth <alexander.unmuth@gmail.com>
2023-12-08 16:48:11 +0100

    Added images for scenes Implemented conditional rendering for scenes and styling

--- frontend/src/pages/ReadyPage/Ready.tsx
+++ frontend/src/pages/ReadyPage/Ready.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Ready.css';
+import '../../App.css';
 
 const characterSelectionPath = '/character-selection';
 
--- frontend/src/pages/StartPage/StartPage.tsx
+++ frontend/src/pages/StartPage/StartPage.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './StartPage.css';
-import "../../App.css";
+import SceneLayout from '../../components/SceneLayout/SceneLayout.tsx';
+import backgroundImage from '../../assets/Start.png';
+import {useLocation} from "react-router-dom";
 
 const StartPage: React.FC = () => {
+    const navLinks = [
+        { path: "/berge", label: "Gehe zu den Bergen" },
+        { path: "/schloss", label: "Gehe zum Schloss" },
+        { path: "/unbekannt", label: "Gehe ins Unbekannte" }
+    ];
+
+    const location = useLocation();
+
     return (
-        <main className="startPageContainer">
-            <header>
-                <h1 className="startPageTitle">Szene Start - Dorf</h1>
-            </header>
-
-            <nav className="choices">
-                <Link to="/berge"><button className="medievalButton">Gehe zu den Bergen</button></Link>
-                <Link to="/schloss"><button className="medievalButton">Gehe zum Schloss</button></Link>
-                <Link to="/unbekannt"><button className="medievalButton">Gehe ins Unbekannte</button></Link>
-            </nav>
-
-            <article className="textWindow">
-                <p>Hallo Held, willkommen in dieser Visual Novel! Dein Abenteuer beginnt in einem kleinen Dorf, das der Ausgangspunkt für deine aufregende Reise ist. Hier hast du die Möglichkeit, dich in ein unvergessliches Abenteuer zu stürzen.</p>
-                <p>Die Frage ist: Wo möchtest du hingehen?</p>
-                <p>Über den oben angezeigten Button kannst du deine Wahl treffen:</p>
-                <ul>
-                    <li>In die Berge: Wage dich in das eisige Terrain, wo rätselhafte Eiskreaturen ihr Unwesen treiben. Ein Ort voller Geheimnisse und Herausforderungen erwartet dich.</li>
-                    <li>Ins Schloss: Besuche das majestätische Schloss, in dem ein Graf residiert. Entdecke, welche Intrigen und Geschichten sich hinter diesen mächtigen Mauern verbergen.</li>
-                    <li>Ins Unbekannte: Oder traust du dich, einen ganz anderen Weg einzuschlagen und dich ins Unbekannte zu begeben? Unvorhersehbare Abenteuer und Überraschungen warten auf dich.</li>
-                </ul>
-                <p>Wähle weise, denn jede Entscheidung wird deinen Weg in diesem Abenteuer prägen. Dein Schicksal liegt in deinen Händen!</p>
-            </article>
-        </main>
+        <SceneLayout
+            title="Szene Start - Dorf"
+            navLinks={navLinks}
+            backgroundImage={backgroundImage}
+        >
+            <p>Aktuelle Location: {location.pathname}</p>
+            <p>Hallo Held, willkommen in dieser Visual Novel! Dein Abenteuer beginnt in einem kleinen Dorf, das der
+                Ausgangspunkt für deine aufregende Reise ist. Hier hast du die Möglichkeit, dich in ein unvergessliches
+                Abenteuer zu stürzen.</p>
+            <p>Die Frage ist: Wo möchtest du hingehen?</p>
+            <p>Über den oben angezeigten Button kannst du deine Wahl treffen:</p>
+            <ul>
+                <li>In die Berge: Wage dich in das eisige Terrain, wo rätselhafte Eiskreaturen ihr Unwesen treiben. Ein
+                    Ort voller Geheimnisse und Herausforderungen erwartet dich.
+                </li>
+                <li>Ins Schloss: Besuche das majestätische Schloss, in dem ein Graf residiert. Entdecke, welche Intrigen
+                    und Geschichten sich hinter diesen mächtigen Mauern verbergen.
+                </li>
+                <li>Ins Unbekannte: Oder traust du dich, einen ganz anderen Weg einzuschlagen und dich ins Unbekannte zu
+                    begeben? Unvorhersehbare Abenteuer und Überraschungen warten auf dich.
+                </li>
+            </ul>
+            <p>Wähle weise, denn jede Entscheidung wird deinen Weg in diesem Abenteuer prägen. Dein Schicksal liegt in
+                deinen Händen!</p>
+        </SceneLayout>
     );
 };
-
 
 export default StartPage;
